954aeda3c5496017523e84167df1e89097b8c4ac
NeOMakinG <valentin.szczupak@cedelabs.io>
2023-11-02 16:21:13 +0100

    fix: tests using the test env of the SDK

--- exchange/getSupportedExchanges.ts
+++ exchange/getSupportedExchanges.ts
@@ -2,7 +2,6 @@ import { cedeSDK } from "../utils";
 
 export const executeMethod = async () => {
   const { getSupportedExchanges } = cedeSDK.api;
-  getSupportedExchanges;
   const url = await getSupportedExchanges();
 
   return url;
