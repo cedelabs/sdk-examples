4a1950f32b6f43c8ff7c0b5784ffac1d346cffdf
Nikita TEREKHOV <nikita.a.terekhov@gmail.com>
2024-09-05 16:07:49 +0200

    chore: update docs

--- prices/getFiatCurrencies.ts
+++
@@ -1,9 +0,0 @@
-import { cedeSDK } from "../utils";
-
-export const executeMethod = async () => {
-  const { getFiatCurrencies } = cedeSDK.api;
-
-  const prices = await getFiatCurrencies();
-
-  return prices;
-};
